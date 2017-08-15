936dd7f212281e51ff2091695011bfe13bd19673
ymranx <ymranx@gmail.com>
2017-08-15 23:40:49 +0530

    SVG line draw

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,35 @@
 
+let mouseDown = false;
+
 export default {
     data: function () {
         return {
             data: null,
             letters: `ABCDEFGHIJKLMNOPQRSTUVWXYZABCDABCDEFGHIJKLMNOPQRSTUVWXYZEFGHIFGHIJKLMNOPQRSTUVWXYZNOABCDEFGHIJKLMNOPABCDEFGHIJKLMNOPQRSTUVWXYZQRSTUVWXYZPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTABCDEFGHIJKLMNOPQRSTUVWXYZUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ`,
             words: ['Aardvark', 'Antelope', 'Donkey', 'Bear', 'Capybara',
-                    'Bison', 'Crow', 'Falcon', 'Elephant', 'Goose']
-        }       
+                'Bison', 'Crow', 'Falcon', 'Elephant', 'Goose']
+        }
     },
     mounted: function () {
+        //
+        var paper = Snap('#appIllustrations');
+        let p1, p2, line;
 
+        $(".app-grid").on("mousedown", ".grid-letter", function (ev) {
+            mouseDown = true;
+            //$(this).index(".grid-letter")
+            p1 = $(this).position();
+            console.log();
+        });
+        $(".app-grid").on("mouseup", ".grid-letter", function (ev) {
+            mouseDown = false;
+            p2 = $(this).position();
+            line = paper.line(p1.left + 7, p1.top + 7, p2.left + 7, p2.top + 7);
+            line.attr({
+                class: "line-1"
+            });
+            console.log($(this).index(".grid-letter"));
+        })
     },
     methods: {
 
--- src/templates/app.vue
+++ src/templates/app.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="app-screen">  
+  <div class="app-screen">
     <section class="app-title"> Wordswipe</section>
     <section class="app-header"> </section>
     <ul class="app-grid list">
+      <svg id='appIllustrations' version="1.1" xmlns="http://www.w3.org/2000/svg">
+      </svg>
       <li v-for="letter in letters" class="grid-letter"> {{letter}}</li>
     </ul>
+  
     <ul class="app-words list">
       <li v-for="word in words" class="grid-word"> {{word}}</li>
     </ul>
+  
     <section class="btn-list">
       <button class="ghost-btn "> New </button>
       <button class="ghost-btn "> Pass </button>
