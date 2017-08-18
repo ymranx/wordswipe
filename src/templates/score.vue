29349cd08f500bbd19d4a325b473691aa992fe12
ymranx <ymranx@gmail.com>
2017-08-18 21:28:46 +0530

    score UI update

--- src/templates/score.vue
+++ src/templates/score.vue
@@ -1,5 +1,18 @@
 <template>
-  <section class="app-score"> </section>
+  <section class="app-score">
+    <span class="score-item">
+      <div class="label">TIME</div>
+      <div class="text">1:59</div>
+    </span>
+    <span class="score-item">
+      <div class="label">SCORE</div>
+      <div class="text">150</div>
+    </span>
+    <span class="score-item">
+      <div class="label">BEST</div>
+      <div class="text">250</div>
+    </span>
+  </section>
 </template>
 
 <script type="text/javascript" src="../js/score.js"></script>
